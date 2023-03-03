99dc1ef5c9706c116cb5d2bf6cc8976eaa4e5810
joyle1 <12914448+joyle1@users.noreply.github.com>
2023-03-03 22:44:12 +0100

    Solution

--- src/main/java/com/devskiller/services/BookSuggestionService.java
+++ src/main/java/com/devskiller/services/BookSuggestionService.java
@@ -1,9 +1,13 @@
 package com.devskiller.services;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.devskiller.model.Author;
 import com.devskiller.model.Book;
+import com.devskiller.model.Genre;
 import com.devskiller.model.Reader;
 
 class BookSuggestionService {
@@ -17,15 +21,56 @@ class BookSuggestionService {
 	}
 
 	Set<String> suggestBooks(Reader reader) {
-		throw new UnsupportedOperationException("Please, implement me");
+		//throw new UnsupportedOperationException("Please, implement me");
+
+		Set<Genre> favGenres = reader.getFavouriteGenres();
+
+		return books.stream()
+				.filter(b -> b.getRating() >= 4)
+				.filter(b -> favGenres.contains(b.getGenre()))
+				.filter(b -> favBooksSameAge(reader).contains(b))
+				.map(b -> b.getTitle())
+				.collect(Collectors.toSet());
+
 	}
 
 	Set<String> suggestBooks(Reader reader, int rating) {
-		throw new UnsupportedOperationException("Please, implement me");
+		//throw new UnsupportedOperationException("Please, implement me");
+
+		Set<Genre> favGenres = reader.getFavouriteGenres();
+
+		return books.stream()
+				.filter(b -> b.getRating() >= rating)
+				.filter(b -> favGenres.contains(b.getGenre()))
+				.filter(b -> favBooksSameAge(reader).contains(b))
+				.map(b -> b.getTitle())
+				.collect(Collectors.toSet());
 	}
 
 	Set<String> suggestBooks(Reader reader, Author author) {
-		throw new UnsupportedOperationException("Please, implement me");
+		//throw new UnsupportedOperationException("Please, implement me");
+
+		Set<Genre> favGenres = reader.getFavouriteGenres();
+
+		return books.stream()
+				.filter(b -> b.getRating() >= 4)
+				.filter(b -> favGenres.contains(b.getGenre()))
+				.filter(b -> favBooksSameAge(reader).contains(b))
+				.filter(b -> b.getAuthor().equals(author))
+				.map(b -> b.getTitle())
+				.collect(Collectors.toSet());
+	}
+
+	private Set<Book> favBooksSameAge(Reader reader) {
+		//return null;
+		Set<Reader> setReaders = readers.stream()
+				.filter(r -> reader.getAge() == r.getAge())
+				.filter(r -> !reader.equals(r))
+				.collect(Collectors.toSet());
+
+		Set<Book> favBooks = new HashSet<>();
+		setReaders.forEach(r -> favBooks.addAll(r.getFavouriteBooks()));
+		return favBooks;
 	}
 
 }
